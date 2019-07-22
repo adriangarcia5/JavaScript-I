e59f7a96251a36f6094887b3aeb1bff9147db92f
Adrian-Garcia <agarc5@hotmail.com>
2019-07-22 17:19:06 -0500

    started stretch nested objects

--- assignments/objects.js
+++ assignments/objects.js
@@ -110,8 +110,10 @@ console.log(internFive.multiplyNums(3,4));
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
-
+const parent = {
+  "name": "Susan",
+  "age": 70,
+}
 // Log the parent object's name
 
 // Log the child's age
